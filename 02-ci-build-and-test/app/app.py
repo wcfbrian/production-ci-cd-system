4f78956c7b133cac22603ca081d535c4396f9ef2
Bee <cfwongbr@icloud.com>
2026-09-08 01:36:28 +0800

    Fix error message in home route to indicate CI/CD production issue

--- 02-ci-build-and-test/app/app.py
+++ 02-ci-build-and-test/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    raise Exception("CI/CD Prod Error")
+    raise Exception("CI/CD Prod broken")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
