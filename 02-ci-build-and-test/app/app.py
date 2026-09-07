0c8619b2f537b22bef5715683e351fc4e4bdb2b1
Bee <cfwongbr@icloud.com>
2026-09-08 01:47:05 +0800

    trigger actions

--- 02-ci-build-and-test/app/app.py
+++ 02-ci-build-and-test/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    raise Exception("CI/CD Prod broken")
+    raise Exception("CI/CD Prod is broken")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
