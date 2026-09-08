9f27e10148fb2dc069a4c9c60642267f4412bac9
Bee <cfwongbr@icloud.com>
2026-09-08 11:14:08 +0800

    Fix typo in home route response message

--- 02-ci-build-and-test/app/app.py
+++ 02-ci-build-and-test/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CICD Prod Running"
+    return "CICD Prod is Running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
