a7cc84dc0fa77eca5ef8b609799392a26cd28f1e
Bee <cfwongbr@icloud.com>
2026-09-08 11:07:24 +0800

    update application code

--- 02-ci-build-and-test/app/app.py
+++ 02-ci-build-and-test/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    raise Exception("CI/CD Prod is broken")
+    return "CICD Prod Running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
